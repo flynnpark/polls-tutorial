61c09ac110aa04bb4610a7b72f2977ec2c5467ad
wphestiraid <wphestiraid@gmail.com>
2017-11-28 10:54:07 +0900

    뷰 테스트 - IndexView 테스트

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.utils import timezone
 from django.urls import reverse
 from django.views import generic
 
@@ -11,7 +12,9 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """ Return the last five published questions. """
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Question
